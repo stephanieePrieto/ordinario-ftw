f5b96096923374ee4583a3e480bcafdf0ce1ac8b
StephanieePrieto <stephanieehpr@gmail.com>
2026-05-29 18:47:27 -0600

    datos.html

---
+++ proyecto/detalle.js
@@ -0,0 +1,29 @@
+// 1. Buscamos los elementos vacíos del HTML usando su id para poder rellenarlos
+
+const textoNombre = document.getElementById("detalle-nombre");
+const textoDescripcion = document.getElementById("detalle-categoria");
+const textoPrecio = document.getElementById("detalle-precio");
+
+const botonVolver = document.getElementById("bton-volver");
+const botonAgregar = document.getElementById("bton-agregar-detalle");
+
+// 2. Funcion para rellenar la pantalla con los datos del maquillaje
+function simularCargaProducto(){
+    textoNombre.innerText = "Paletade Sombras Neón";
+    textoDescripcion.innerText = "Ojos";
+    textoPrecio.innerText = "350";
+}
+
+function regresarAlCatalogo(){
+    window.location.href = "catalogo.html";
+}
+
+function agregarBolsa(){
+    alert("¡Paleta de Sombras Neón añadida a tu bolsa de compras!");
+    window.location.href = "catalogo.html";
+}
+
+botonVolver.addEventListener("click", regresarAlCatalogo);
+botonAgregar.addEventListener("click", agregarBolsa);
+
+simularCargaProducto();
